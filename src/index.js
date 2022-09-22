43f89a7f7c7133619c348825e039285805f1c67b
Imanzuher <imoojaf@gmail.com>
2022-09-23 01:10:07 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 const init = () => {
-  
-}
-
-document.addEventListener('DOMContentLoaded', init);
+  const inputForm = document.querySelector("form");
+  inputForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const input = document.querySelector("input#searchByID");
+    fetch(`http://localhost:3000/movies/${input.value}`)
+      .then((res) => res.json())
+      .then((data) => {
+        const title = document.querySelector("section#movieDetails h4");
+        const summary = document.querySelector("section#movieDetails p");
+        title.innerHTML = data.title;
+        summary.innerHTML = data.summary;
+      });
+  });
+};
+document.addEventListener("DOMContentLoaded", init);
